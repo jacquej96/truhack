f463eec57228b6af7f741817e3b88ea9d765a52c
VelocityKnight <32784540+VelocityKnight@users.noreply.github.com>
2018-04-07 15:08:34 -0500

    Add files via upload

--- usercheck.php
+++ usercheck.php
@@ -3,16 +3,21 @@
           $email = $_POST['email'];
           $password = $_POST['password'];
           try {
-          $conn = new PDO("mysql:host=mysql.truman.edu;dbname=cef6418CS315", "cef6418", "ohbiethe");
-          $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $conn = new PDO("mysql:host=mysql.truman.edu;dbname=cef6418CS315", "cef6418", "ohbiethe");
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-          $stmt = $conn->prepare("SELECT *
-          FROM studentHack
-          WHERE email = :email AND password = :password");
-          $stmt->bindParam(':email', $email);
-          $stmt->bindParam(':password', $password);
+            $stmt = $conn->prepare("SELECT *
+            FROM studentHack
+            WHERE email = :email AND password = :password");
+            $stmt->bindParam(':email', $email);
+            $stmt->bindParam(':password', $password);
 
-          $stmt->execute();
+            $stmt->execute();
+
+            $count = mysql_num_rows($stmt);
+            if($count > 1){
+              header("Location: main.php");
+            }
           }
           catch(PDOException $e)
           {
