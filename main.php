73dd7f0045d08d50173c042761c7eeda42cb93ed
Claire Fuchs <cfuchs981@gmail.com>
2018-04-07 08:35:27 -0500

    main sketelton

---
+++ main.php
@@ -0,0 +1,65 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>CS Registration</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrap.min.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="style.css" rel="stylesheet">
+
+  </head>
+
+  <body id="page-top">
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav" >
+      <a class="navbar-brand js-scroll-trigger" >
+        <span class="d-none d-lg-block">
+          <img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="img/profile.jpg" alt="">
+        </span>
+      </a>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav">
+          <li class="nav-item name"> Name</li>
+          <li class="nav-item major">Major : Minor </li>
+          <li class="nav-item grad">Graduation Date</li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="container-fluid p-0" >
+
+      <section class="tinder-section p-1 p-lg-5 d-flex d-column" style="width: 65%;">
+        <div class="my-auto" style="width:100%;padding: 20px;text-align:center;border: 5px solid black;">
+          <div class="subheading mb-5">NAME OF CLASS · COURSE NUMBER</div>
+          <p class="mb-5" id="descrip">(picture??)Class description goes here</p>
+          <p class="mb-5" id="capteach">Capacity, teacher etc here </p> 
+          <p class="mb-5" style="font-weight:bold;"id="type">Class type and credits</p>
+          <div class="button-hold">
+          	<button>Placeholdyes</button>
+          	<button>Placeholdno</button>
+          </div>
+         
+        </div>
+
+      </section>
+
+  </div>
+
+  <!--TODO add table display of selected classes to right wing of page. NOTE: we don't need a printing page. Let's just do an 'email' button or some such that will send the current table values, no need to be extra--> 
+
+
+
+<div class="hiddens"></div> <!--this is where jon can dump data as discussed if we decide to go through that method. He should auto generate this div himself.--> 
+
+      
+
+
+  </body>
+
+</html>
